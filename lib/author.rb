797fb25ccd08c3b32935b71ed70e284a3e540c4b
Romario Gustave <romagustave10@gmail.com>
2020-01-03 20:39:11 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,16 +1,26 @@
 class Author 
-  attr_accessor :name, :posts, :artist, :title
+  
+  attr_accessor :name 
   
   def initialize(name)
     @name = name 
   end 
   
-  def add_post(author)
-    author.artist = self
+  def posts 
+    Post.all 
   end 
   
-  def add_post_by_title(title)
-    
+  def add_post(post)
+    post.author = self 
   end 
+  
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    add_post(post)
+  end 
+  
+  def self.post_count 
+    Post.all.length  
+  end 
+  
 end 
-  
